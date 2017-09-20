3386a8158c726e84c9a5ded23d68f5dc11469560
Mike Pheasant <mike.pheasant@ephox.com>
2017-09-20 15:39:27 +1000

    cleanup

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-((emacs-lisp-mode
-  (eval . (setq flycheck-emacs-lisp-load-path load-path
-                flycheck-disabled-checkers '(emacs-lisp-checkdoc)))
-  (eval . (flycheck-color-mode-line-mode -1))))
